9d52d765a69c98bfa3d8f8bd8b096bc1c31d8a73
unimatrix2 <dnny.nis@gmail.com>
2022-01-19 17:37:33 -0300

    implemented parsing service

--- src/bin/commands/parse.command.ts
+++ src/bin/commands/parse.command.ts
@@ -1,9 +1,11 @@
 import { Command } from 'commander';
 
+import { parser } from '../../services/parse.service';
+
 const program = new Command();
 
 program
     .argument('<file>', 'Path of the file to be parsed')
-    .action((file) => console.log(file));
+    .action(async (file: string) => await parser(file));
 
 program.parse(process.argv);
