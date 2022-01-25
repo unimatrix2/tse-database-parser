a286a3c66671b5e0cd502ef9773e083676c45cea
unimatrix2 <dnny.nis@gmail.com>
2022-01-25 20:14:01 -0300

    added util lib

---
+++ src/lib/util.lib.ts
@@ -0,0 +1,35 @@
+import { ICandidate } from '../..';
+import AppError from '../error/AppError';
+
+export const batchMaker = (data: ICandidate[]): ICandidate[][] => {
+	const size: number = 10000;
+	const length: number = Math.ceil(data.length / size);
+	const documentArray: ICandidate[][] = [];
+	for (let i = 0; i < length; i += 1) {
+		documentArray.push(data.slice(i * size, i * size + size));
+	}
+	return documentArray;
+};
+
+export const insertionErrorHandler = (
+	error: any,
+	method: string,
+	step?: string
+): void => {
+	if (error.keyValue) {
+		console.log(error); // This is here because this specific error case will be handled specially and I need to know what error type is being thrown
+		throw new AppError({
+			message: error.message,
+			method,
+			module: 'ImporterLib',
+			step,
+			field: error.keyValue,
+		});
+	}
+	throw new AppError({
+		message: error.message,
+		method,
+		module: 'ImporterLib',
+		step
+	});
+};
