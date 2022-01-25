01b4fa8bcf8843108e059c0814b0607d2857f1b7
unimatrix2 <dnny.nis@gmail.com>
2022-01-25 18:03:52 -0300

    added argument,  option & fixed parse call

--- src/bin/commands/parse.command.ts
+++ src/bin/commands/parse.command.ts
@@ -5,7 +5,13 @@ import { parser } from '../../services/parse.service';
 const program = new Command();
 
 program
-    .argument('<file>', 'Path of the file to be parsed')
-    .action(async (file: string) => await parser(file));
+    .argument('<file>', 'Path of the file to be parsed (needs absolute path)')
+    .argument('<MongoDB URI>', 'The MongoDB connection URI for parsing')
+    .option('-u, --update', 'Indicates you wish to update an existing database')
+    .action(async (
+        file: string,
+        uri: string,
+        options: { [key: string]: boolean }
+    ): Promise<void> => !options.update ? await parser(file, uri) : console.log(options));
 
-program.parse(process.argv);
+program.parseAsync(process.argv);
