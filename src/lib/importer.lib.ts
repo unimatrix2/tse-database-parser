c1fe7a20f00b0b76af2de5d7594d1d002cdb4094
unimatrix2 <dnny.nis@gmail.com>
2022-01-25 18:31:24 -0300

    linted

--- src/lib/importer.lib.ts
+++ src/lib/importer.lib.ts
@@ -2,9 +2,7 @@ import AppError from '../error/AppError';
 import { ICandidate } from '../..';
 import { batchMaker, insertManyBatch, singleSaveLoop } from './db.lib';
 
-export const singleDocumentImport = async (
-	data: ICandidate[]
-) => {
+export const singleDocumentImport = async (data: ICandidate[]) => {
 	try {
 		await singleSaveLoop(data);
 	} catch (error: any) {
@@ -17,9 +15,7 @@ export const singleDocumentImport = async (
 	}
 };
 
-export const batchDocumentImport = async (
-	data: ICandidate[]
-) => {
+export const batchDocumentImport = async (data: ICandidate[]) => {
 	try {
 		const batches = batchMaker(data);
 		await insertManyBatch(batches);
