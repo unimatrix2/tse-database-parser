ce0d30c740193318db199c77188e1b448e8ff059
unimatrix2 <dnny.nis@gmail.com>
2022-01-25 00:46:37 -0300

    typed variable

--- src/bin/tseparser.ts
+++ src/bin/tseparser.ts
@@ -2,7 +2,7 @@
 import { Command } from 'commander';
 import pkg from '../../package.json';
 
-const program = new Command();
+const program: Command = new Command();
 
 program
     .version(pkg.version)
