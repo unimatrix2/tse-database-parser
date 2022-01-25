b60119832f309aca6178efee0d3db196d99088f0
unimatrix2 <dnny.nis@gmail.com>
2022-01-25 00:43:46 -0300

    implemented optional params

--- src/error/AppError.ts
+++ src/error/AppError.ts
@@ -2,14 +2,25 @@ import 'colors';
 
 import { ErrorParams } from '../../@types/tseparser/src/error';
 
+const optionalParamsMapper = (
+	optional: string | number | undefined
+): boolean => Boolean(optional) || String(optional) === '0';
+
+
 export default class AppError extends Error {
 	method: string;
 	module: string;
+	step?: string;
+	index?: number;
+	field?: string;
 
 	constructor(params: ErrorParams) {
 		super();
 		this.message = params.message || 'Application Error'.red.underline;
 		this.method = params.method || 'Unknown Method'.red;
         this.module = params.module || 'Unknown Module'.red;
+		optionalParamsMapper(params.step) ? this.step = params.step : null;
+		optionalParamsMapper(params.index) ? this.index = params.index : null;
+		optionalParamsMapper(params.field) ? this.field = params.field : null;
 	}
 }
