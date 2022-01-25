11d9d8d33bbf55a24a28830e9922c655b863bc38
unimatrix2 <dnny.nis@gmail.com>
2022-01-25 03:42:33 -0300

    added better error handling

--- src/lib/parser.lib.ts
+++ src/lib/parser.lib.ts
@@ -2,6 +2,8 @@ import Papa from 'papaparse';
 import fs from 'fs';
 import 'colors';
 
+import AppError from '../error/AppError';
+
 export const parse = (url: string) => {
 	return new Promise((resolve, reject) => {
 		Papa.parse(fs.createReadStream(url), {
@@ -11,6 +13,14 @@ export const parse = (url: string) => {
 			complete: (results) => {
 				console.log(`${String(results.data.length).green} entries parsed`);
 				console.log(`${String(results.errors.length).green} parsing errors`);
+				if (results.errors.length > 0) {
+					reject(new AppError({
+						message: 'Errors were found on the table, and no data was imported into the database',
+						method: 'parse',
+						module: 'ParserLib',
+						field: JSON.stringify(results.errors)
+					}))
+				}
 				resolve(results.data);
 			},
 			error: (err) => reject(err),
