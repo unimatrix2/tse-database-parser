89f4b33f006d714a353216f0974f3e8210f4f5f4
unimatrix2 <dnny.nis@gmail.com>
2022-01-25 17:35:39 -0300

    removed cache solution

--- src/lib/store.lib.ts
+++
@@ -1,9 +0,0 @@
-import Conf from 'conf';
-
-const conf = new Conf();
-
-export const setCache = (parsedData: any) => conf.set('cache', parsedData);
-
-export const getCache = () => conf.get('cache');
-
-export const clearCache = () => conf.clear();
