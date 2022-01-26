d342b653baeb5e748c9ce2559dc13f8b54f9f547
unimatrix2 <dnny.nis@gmail.com>
2022-01-26 00:32:57 -0300

    add log & error handling for error on parse

--- src/lib/parser.lib.ts
+++ src/lib/parser.lib.ts
@@ -13,23 +13,36 @@ export const parse = (url: string): Promise<ICandidate[]> => {
 			skipEmptyLines: true,
 			complete: (results: IParseResult) => {
 				console.log(`${String(results.data.length).green} entries parsed`);
-				console.log(`${String(results.errors.length).green} parsing errors`);
+				const resultsErrorsLength = results.errors.length;
+				console.log(
+					`${
+						resultsErrorsLength < 1
+							? String(resultsErrorsLength).green
+							: String(results.errors.length).red
+					} parsing errors`
+				);
 				if (results.errors.length > 0) {
-					reject(new AppError({
-						message: 'Errors were found on the table, and no data was imported into the database',
-						method: 'parse',
-						module: 'ParserLib',
-						step: 'Complete',
-						field: JSON.stringify(results.errors)
-					}))
+					reject(
+						new AppError({
+							message:
+								'Errors were found on the table, and no data was imported into the database',
+							method: 'parse',
+							module: 'ParserLib',
+							step: 'Complete',
+							field: JSON.stringify(results.errors),
+						})
+					);
 				}
 				resolve(results.data);
 			},
-			error: (err) => reject(new AppError({
-				message: err.message,
-				method: 'Parse',
-				module: 'ParserLib'
-			})),
+			error: (err) =>
+				reject(
+					new AppError({
+						message: err.message,
+						method: 'Parse',
+						module: 'ParserLib',
+					})
+				),
 		});
 	});
 };
