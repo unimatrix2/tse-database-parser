020a540b7aa35ac67779b21a69af3ca922ad62ba
unimatrix2 <dnny.nis@gmail.com>
2023-12-29 22:19:56 -0300

    removed commented code

--- src/bin/tseparser.ts
+++ src/bin/tseparser.ts
@@ -7,8 +7,6 @@ const program: Command = new Command();
 program
     .version(pkg.version)
     .description('A simple CLI parser to crunch public CSV data from the Supreme Electoral Court in Brazil.')
-    .command('parse', 'Handle parsing operations', { executableFile: './commands/parse.command' })
-    //.command('convert', 'Handle conversion operations', { executableFile: './commands/convert.command' })
-    //.command('import', 'Handles DB import operations', { executableFile: './commands/import.command' });
+    .command('parse', 'Handle parsing operations', { executableFile: './commands/parse.command' });
 
 program.parse(process.argv);
