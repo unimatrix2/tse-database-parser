f8e95506facc8fcd073d78456cc468ad529c73e7
unimatrix2 <dnny.nis@gmail.com>
2024-05-04 18:11:40 -0300

    refactor: if multiple years need to download, everything is parallel now

--- src/services/download.service.ts
+++ src/services/download.service.ts
@@ -5,22 +5,29 @@ import { TSE_BASE_FILE_NAME, TSE_CDN_BASE_URL, TSE_DOWNLOAD_FILE_EXT } from '../
 
 export async function download(years: string, path: string) {
   const parsedYears = years.includes(',') ? years.split(',') : [years];
+  parsedYears.forEach(validateYear);
 
-  for (const year of parsedYears) {
+  const requests = [...parsedYears].map(year => {
     const url = `${TSE_CDN_BASE_URL}${TSE_BASE_FILE_NAME}${year}${TSE_DOWNLOAD_FILE_EXT}`;
+    return fetch(url).then(async response => {
+      if (!response.ok) throw new Error(`${log.error}Failed to download data from url: ${url}`);
+      
+      const filePath = `${path}/${TSE_BASE_FILE_NAME}${year}${TSE_DOWNLOAD_FILE_EXT}`;
+      const writeStream = createWriteStream(filePath);
+      console.log(log.info + 'Saving data to:', filePath);
+      const arrayBuffer = await response.arrayBuffer();
+      const buffer = Buffer.from(arrayBuffer);
+      writeStream.write(buffer);
+      console.log(log.success + 'Data saved to:', filePath);
+    });
+  });
 
-    console.log(log.info + 'Downloading data from', url);
-    const response = await fetch(url);
-    if (!response.ok) throw new Error(`${log.error}Failed to download data from url: ${url}`);
-    console.log(log.success + 'Downloaded data from', url);
+  await Promise.all(requests);
+  console.log(log.success + 'All data saved to:', path);
+}
 
-    const filePath = `${path}/${TSE_BASE_FILE_NAME}${year}${TSE_DOWNLOAD_FILE_EXT}`;
-    const writeStream = createWriteStream(filePath);
-
-    console.log(log.info + 'Saving data to:', filePath);
-    const arrayBuffer = await response.arrayBuffer();
-    const buffer = Buffer.from(arrayBuffer);
-    writeStream.write(buffer);
-    console.log(log.success + 'Data saved to:', filePath);
-  }
+function validateYear(year: string) {
+  if (!year.match(/\d{4}/)) throw new Error(`${log.error}Invalid year format: ${year}`);
+  const yearNumber = Number(year);
+  if (yearNumber < 1998 || yearNumber > 2022) throw new Error(`${log.error}Invalid year: ${year}`);
 }
